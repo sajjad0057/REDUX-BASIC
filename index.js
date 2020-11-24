19ee55299406aefff290df27140ad25c9ba2d76f
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-25 01:50:50 +0600

    Dispatching Actions

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const redux = require('redux');
+const redux = require("redux");
 //console.log('test case----->',redux);
-
 
 // state
 const initState = {
-    num : 0,
-}
+  num: 0,
+};
 
-// Reducer 
-const rootReducer = (state=initState,action) =>{
-    return state
-}
+// Reducer
+const rootReducer = (state = initState, action) => {
+  if (action.type === "INC_NUM") {
+    //console.log({...initState});
+
+    return {
+      ...state,            // using spread operator for creating a state copy to updateing state.
+      num: state.num + 1,
+    };
+  } else if (action.type === "ADD_NUM") {
+    return {
+      ...state,
+      num: state.num + action.value,
+    };
+  }
+  return state;
+};
 
 // Store
-
-const store = redux.createStore(rootReducer);  // store  accept a  Reducer function
+// Creating a Store below :
+const store = redux.createStore(rootReducer); // store  accept a  Reducer function
 //console.log(store);
-
 
 // getState() function will show always current or updated state
 
-console.log('Current State : ',store.getState());
+//console.log("Current State : ", store.getState());
 
+// Dispatching Action :     dispatch() function will send "action" to root for performing something.
 
+store.dispatch({
+  type: "INC_NUM",
+});
+
+store.dispatch({
+  type: "ADD_NUM",
+  value: 34,
+});
+console.log(store.getState())
