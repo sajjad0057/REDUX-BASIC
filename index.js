73e9f442864bf915606f3a8834a365ea87b32d22
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-25 02:09:01 +0600

    Store Subscription

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const redux = require("redux");
 //console.log('test case----->',redux);
 
+
+
+
 // state
 const initState = {
   num: 0,
 };
+
+
+
 
 // Reducer
 const rootReducer = (state = initState, action) => {
@@ -15,7 +21,15 @@ const rootReducer = (state = initState, action) => {
       ...state,            // using spread operator for creating a state copy to updateing state.
       num: state.num + 1,
     };
-  } else if (action.type === "ADD_NUM") {
+  } 
+  else if (action.type==="DEC_NUM"){
+      return{
+          ...state,
+          num : state.num - 1
+      }
+
+  }
+  else if (action.type === "ADD_NUM") {
     return {
       ...state,
       num: state.num + action.value,
@@ -24,23 +38,54 @@ const rootReducer = (state = initState, action) => {
   return state;
 };
 
+
+
 // Store
 // Creating a Store below :
 const store = redux.createStore(rootReducer); // store  accept a  Reducer function
 //console.log(store);
 
-// getState() function will show always current or updated state
 
-//console.log("Current State : ", store.getState());
+
+
+
+// subscribe() will execute automatically for performing every action
+store.subscribe(()=>{     //subscribe function will accept a another function as a parameter.
+    console.log("Comming result from subscribe func()--->",store.getState());
+})
+
+
+
+
+
+// getState() function will show always current or updated state
+//console.log("Current State : ", store.getState())
+
+
+
+
+
 
 // Dispatching Action :     dispatch() function will send "action" to root for performing something.
 
+  
 store.dispatch({
   type: "INC_NUM",
 });
 
 store.dispatch({
-  type: "ADD_NUM",
-  value: 34,
-});
-console.log(store.getState())
+    type: "ADD_NUM",
+    value: 34,
+  });
+
+
+store.dispatch({
+    type: "DEC_NUM",
+  });
+
+
+
+
+
+
+
